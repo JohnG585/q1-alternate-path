e104b68baccccb73f729cf69f41c94efa566037b
JohnG585 <jgallagher585@gmail.com>
2017-02-27 16:38:46 -0700

    redid game file

--- js/states/Game.js
+++ js/states/Game.js
@@ -2,207 +2,199 @@ var g44Game = g44Game || {};
 
 g44Game.GameState = {
 
-  init: function() {
-    this.game.physics.arcade.gravity.y = 1000;
+   init: function() {
 
-    this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-    this.scale.pageAlignHorizontally = true;
-    this.scale.pageAlignVertically = true;
+       this.game.physics.arcade.gravity.y = 1000;
 
-    this.cursors = this.game.input.keyboard.createCursorKeys();
 
-    this.game.world.setBounds(0, 0, 1100, 700);
+       this.cursors = this.game.input.keyboard.createCursorKeys();
 
-    this.RUNNING_SPEED = 180;
-    this.JUMPING_SPEED = 560;
-    this.BOUNCING_SPEED = 150;
-  },
+       this.game.world.setBounds(0, 0, 1100, 700)
 
-  preload: function() {
-    this.load.spritesheet('fire', 'assets/images/fire_spritesheet.png', 20, 21, 5, 1, 1);
-    this.load.spritesheet('player', 'assets/images/craig_walk.png', 52, 57, 5, 1, 1);
+       this.RUNNING_SPEED = 180;
+       this.JUMPING_SPEED = 560;
+       this.BOUNCING_SPEED = 150;
 
-    // load json
+   },
 
-    this.load.text('level', 'assets/data/level.json');
-  },
+   preload: function () {
 
-  create: function() {
-    this.background = this.game.add.sprite(0, 0, 'background');
+       this.load.spritesheet('fire', 'assets/images/fire_spritesheet.png', 20, 21, 5, 1, 1);
+       this.load.spritesheet('player', 'assets/images/craig_walk.png', 52, 57, 5, 1, 1);
 
-    // originally I had created seperate elements, but along the way learned of
-    // "groups"
+       //load json
+       this.load.text('level', 'assets/data/level.json');
 
-        var groundData = [{
-                'x': 0,
-                'y': 627
-            },
-            {
-                'x': 350,
-                'y': 627
-            },
-            {
-                'x': 500,
-                'y': 627
-            },
-            { 'x': 850, 'y': 627 }
-        ];
+   },
 
-        // parsing json
-    this.levelData = JSON.parse(this.game.cache.getText('level'));
+   create: function () {
 
-    this.grounds = this.add.group();
-    this.grounds.enableBody = true;
+       this.background = this.game.add.sprite(0, 0, 'background');
+        //originally I had created seperate elements, but along the way learned of "groups"
 
-    groundData.forEach(function(element) {
-      this.grounds.create(element.x, element.y, 'ground');
-    }, this);
+         var groundData = [
+           {'x':0, 'y':627},
+           {'x':350, 'y':627},
+           {'x':500, 'y':627},
+           {'x':850, 'y':627},
+               ];
 
-    this.grounds.setAll('body.immovable', true);
-    this.grounds.setAll('body.allowGravity', false);
+       //parsing json
+       this.levelData = JSON.parse(this.game.cache.getText('level'));
 
-    this.platforms = this.add.group();
-    this.platforms.enableBody = true;
+       this.grounds = this.add.group();
+       this.grounds.enableBody = true;
 
-    this.levelData.platformData.forEach(function(element) {
-      this.platforms.create(element.x, element.y, 'platform');
-    }, this);
+       groundData.forEach(function(element) {
+           this.grounds.create(element.x, element.y, 'ground')
+       }, this);
 
-    this.platforms.setAll('body.immovable', true);
-    this.platforms.setAll('body.allowGravity', false);
+       this.grounds.setAll('body.immovable', true);
+       this.grounds.setAll('body.allowGravity', false);
 
-      // fires
+       this.platforms = this.add.group();
+       this.platforms.enableBody = true;
 
-    this.fires = this.add.group();
-    this.fires.enableBody = true;
+       this.levelData.platformData.forEach(function(element) {
+           this.platforms.create(element.x, element.y, 'platform')
+       }, this);
 
-    var fire;
+       this.platforms.setAll('body.immovable', true);
+       this.platforms.setAll('body.allowGravity', false);
 
-    this.levelData.fireData.forEach(function(element) {
-      fire = this.fires.create(element.x, element.y, 'fire');
-      fire.animations.add('fire', [0, 1], 4, true);
-      fire.play('fire');
-    }, this);
+       //fires
 
-    this.fires.setAll('body.allowGravity', false);
+       this.fires = this.add.group();
+       this.fires.enableBody = true;
 
-    // beer
+       var fire;
+       this.levelData.fireData.forEach(function(element) {
+           fire=this.fires.create(element.x, element.y, 'fire');
+           fire.animations.add('fire', [0, 1], 4, true);
+           fire.play('fire');
+       }, this);
 
-    this.beers = this.add.group();
-    this.beers.enableBody = true;
-    this.game.physics.arcade.enable(this.beers);
+       this.fires.setAll('body.allowGravity', false);
 
-    this.levelData.beerCase.forEach(function(element) {
-      this.beers.create(element.x, element.y, 'beer');
-    }, this);
-    this.beers.setAll('body.allowGravity', false);
+       //beer
 
-      // goal
+       this.beers = this.add.group();
+       this.beers.enableBody=true;
+       this.game.physics.arcade.enable(this.beers);
 
-    this.goal = this.add.sprite(this.levelData.goal.x, this.levelData.goal.y, 'goal');
-    this.game.physics.arcade.enable(this.goal);
-    this.goal.body.allowGravity = false;
+       this.levelData.beerCase.forEach(function(element) {
+           this.beers.create(element.x, element.y, 'beer');
+       }, this);
+       this.beers.setAll('body.allowGravity', false);
 
-    this.player = this.add.sprite(this.levelData.playerStart.x, this.levelData.playerStart.y, 'player', 2);
-    this.player.anchor.setTo(0.5);
-    this.player.animations.add('walking', [0, 1,],6, true);
-    this.game.physics.arcade.enable(this.player);
-    this.player.body.collideWorldBounds = true;
+       //goal
 
-    this.game.camera.follow(this.player);
+       this.goal = this.add.sprite(this.levelData.goal.x, this.levelData.goal.y, 'goal')
+       this.game.physics.arcade.enable(this.goal);
+       this.goal.body.allowGravity = false;
 
-    this.barrels = this.add.group();
-    this.barrels.enableBody = true;
-    this.createBarrel();
-    this.barrelCreate = this.game.time.events.loop(Phaser.Timer.SECOND * this.levelData.barrelRelease, this.createBarrel, this)
+       this.player = this.add.sprite(this.levelData.playerStart.x, this.levelData.playerStart.y, 'player', 2);
+       this.player.anchor.setTo(0.5);
+       this.player.animations.add('walking', [0, 1,], 6, true);
+       this.game.physics.arcade.enable(this.player);
+       this.player.body.collideWorldBounds = true;
 
-        // this.collectBeersLabel = this.add.text(6, 30, "0")
-  },
+       this.game.camera.follow(this.player);
 
-  update: function() {
-      var score = 0;
-      var scoreText = this.game.add.text(500, 50, 'score:', {
-        fontSize: '32px',
-        fill: '#000'
-      });
+       this.barrels = this.add.group();
+       this.barrels.enableBody = true;
+       this.createBarrel();
+       this.barrelCreate = this.game.time.events.loop(Phaser.Timer.SECOND * this.levelData.barrelRelease, this.createBarrel, this)
 
-    this.game.physics.arcade.collide(this.player, this.grounds);
-    this.game.physics.arcade.collide(this.player, this.platforms);
+       // this.collectBeersLabel = this.add.text(6, 30, "0")
+   },
 
-    this.game.physics.arcade.collide(this.barrels, this.grounds);
-    this.game.physics.arcade.collide(this.barrels, this.platforms);
-    this.game.physics.arcade.overlap(this.player, this.barrels, this.hitBarrel, null, this);
+   update: function () {
 
-    this.game.physics.arcade.overlap(this.player, this.fires, this.killsPlayer, null, this);
-    this.game.physics.arcade.overlap(this.player, this.goal, this.wins, null, this);
-    this.game.physics.arcade.overlap(this.player, this.beers, collectBeers, null, this);
+       this.score = 0;
+       this.scoreText = this.game.add.text(500, 50, 'score:', { fontSize: '32px', fill: '#000' });
 
-    this.player.body.velocity.x = 0;
+       this.game.physics.arcade.collide(this.player, this.grounds);
+       this.game.physics.arcade.collide(this.player, this.platforms);
 
-    if (this.cursors.left.isDown) {
-      this.player.body.velocity.x = -this.RUNNING_SPEED;
-      this.player.play('walking');
-      this.player.scale.setTo(-1, 1);
-    }
-    else if (this.cursors.right.isDown) {
-      this.player.body.velocity.x = this.RUNNING_SPEED;
-      this.player.play('walking');
-      this.player.scale.setTo(1, 1);
-    }
-    else {
-      this.player.animations.stop();
-      this.player.frame = 2;
-    }
-    if (this.cursors.up.isDown && this.player.body.touching.down) {
-      this.player.body.velocity.y = -this.JUMPING_SPEED;
-    }
-    var barrel;
-    this.barrels.forEach(function(element) {
-      if (element.x > 280 && element.x < 290 && element.y > 160) {
-        element.kill();
-      }
-    }, this);
+       this.game.physics.arcade.collide(this.barrels, this.grounds);
+       this.game.physics.arcade.collide(this.barrels, this.platforms);
+       this.game.physics.arcade.collide(this.player, this.barrels, hitBarrel, null, this);
 
-    function collectBeers(player, beers) {
-      beers.kill();
-      score += 10;
-      scoreText.text += score;
-    }
-  },
+       this.game.physics.arcade.overlap(this.player, this.fires, this.killsPlayer, null, this);
+       this.game.physics.arcade.overlap(this.player, this.goal, this.wins, null, this);
+       this.game.physics.arcade.overlap(this.player, this.beers, collectBeers, null, this);
 
-    killsPlayer: function() {
-      var death = $("<div class='text-centered'>'OH NOES! <br> CRAIG DIED!'</div>");
-      $('body').append(death);
-      Materialize.toast(death, 5000);
-      this.game.state.start('Game');
-    },
-  hitBarrel: function(player, barrels) {
-    if (barrels.body.touching.up) {
+       this.player.body.velocity.x = 0;
+
+       if(this.cursors.left.isDown) {
+           this.player.body.velocity.x = -this.RUNNING_SPEED;
+           this.player.play('walking');
+           this.player.scale.setTo(-1,1);
+       }
+       else if(this.cursors.right.isDown) {
+           this.player.body.velocity.x = this.RUNNING_SPEED;
+           this.player.play('walking');
+           this.player.scale.setTo(1,1);
+       }
+       else {
+           this.player.animations.stop();
+           this.player.frame = 2;
+       }
+       if (this.cursors.up.isDown && this.player.body.touching.down) {
+           this.player.body.velocity.y = -this.JUMPING_SPEED;
+       }
+       var barrel;
+       this.barrels.forEach(function(element) {
+
+
+           if(element.x > 280 && element.x <290 && element.y > 160) {
+               element.kill();
+           }
+
+       },this)
+
+   function collectBeers (player, beers){
+   beers.kill();
+
+  this.score += 10;
+  this.scoreText.text += this.score;
+
+   }
+      function hitBarrel(player, barrels) {
+    if(barrels.body.touching.up) {
       barrels.kill();
-      score += 10;
-      scoreText.text += score;
+      // this.score += 10;
+      // this.scoreText.text += score;
       player.body.velocity.y = -this.BOUNCING_SPEED;
     }
     else {
-      this.killsPlayer();
+     this.killsPlayer();
     }
-  },
-  wins: function() {
-    this.game.state.start('Game');
-    alert('You Won!');
-  },
-  createBarrel: function() {
-    var barrel = this.barrels.getFirstExists(false);
+   }
+   },
+   killsPlayer: function() {
+       var death = $("<div class='text-centered'>'OH NOES! <br> CRAIG DIED!'</div>");
+       $('body').append(death);
+       Materialize.toast(death, 5000)
+       this.game.state.start('Game');
+   },
+   wins: function() {
+       this.game.state.start('Game');
+        alert("You Won!");
+   },
+   createBarrel: function() {
+       var barrel = this.barrels.getFirstExists(false);
 
-    if (!barrel) {
-      barrel = this.barrels.create(0, 0, 'barrel');
-    }
+       if(!barrel) {
+           barrel = this.barrels.create(0,0, 'barrel');
+       }
 
-    barrel.body.collideWorldBounds = true;
-    barrel.body.bounce.set(1, 0);
+       barrel.body.collideWorldBounds = true;
+       barrel.body.bounce.set(1,0);
 
-    barrel.reset(this.levelData.goal.x, this.levelData.goal.y);
-    barrel.body.velocity.x = this.levelData.barrelSpeed;
-    barrel.body.bounce.set(1);
-  },
+       barrel.reset(this.levelData.goal.x, this.levelData.goal.y);
+       barrel.body.velocity.x = this.levelData.barrelSpeed;
+       barrel.body.bounce.set(1);
+   },
 };
